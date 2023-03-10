f5333fd2b1efcf9f8b45b027b40865f56df92ac4
Lien Bobby <vienphatbobby.lien@students.bfh.ch>
2023-03-10 23:54:08 +0100

    update

--- src/main/java/KeycloakAPI/KeycloakAPI/security/WebConfig.java
+++ src/main/java/KeycloakAPI/KeycloakAPI/security/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,24 +20,29 @@ public class WebConfig implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	            .allowedOrigins("https://192.168.1.129:4200")
+	            .allowedOrigins("https://192.168.1.129")
 	            .allowedMethods("*")
 	            .allowedHeaders("*")
 	            .allowCredentials(true);
 	    }
 	 
-	 @Bean
-	    public CorsConfigurationSource corsConfigurationSource() {
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.setAllowCredentials(true);
-	        configuration.setAllowedOrigins(Arrays.asList("https://192.168.1.129:4200"));
-	        configuration.setAllowedMethods(Arrays.asList("*"));
-	        configuration.setAllowedHeaders(Arrays.asList("*"));
-	        configuration.setExposedHeaders(Arrays.asList(HttpHeaders.SET_COOKIE));
-
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-
-	        return source;
-	    }
+	   @Bean
+	    public CorsFilter corsFilter() {
+	    	String localIP = "https://192.168.1.129";
+			CorsConfiguration corsConfiguration = new CorsConfiguration();
+			corsConfiguration.setAllowCredentials(true);
+			//corsConfiguration.setAllowedOrigins(Arrays.asList("http://10.2.109.120:4200"));
+			corsConfiguration.setAllowedOrigins(Arrays.asList(localIP, localIP+":4200","https://localhost:4200"));
+			//corsConfiguration.setAllowedOriginPatterns(Arrays.asList("https://10.2.109.120"));
+			corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+					"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+					"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+			corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+					"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+			corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+			UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+			urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+			return new CorsFilter(urlBasedCorsConfigurationSource);
+		}
+	   
 }
