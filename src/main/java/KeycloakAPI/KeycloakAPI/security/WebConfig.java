eaf225c6c67c5c73a30626af103f9edb88c94238
Lien Bobby <vienphatbobby.lien@students.bfh.ch>
2023-03-10 23:13:44 +0100

    update

--- src/main/java/KeycloakAPI/KeycloakAPI/security/WebConfig.java
+++ src/main/java/KeycloakAPI/KeycloakAPI/security/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("https://192.168.1.129")
+		.allowedOrigins("https://192.168.1.129:4200")
 		.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
 		.allowCredentials(true);
 	}
